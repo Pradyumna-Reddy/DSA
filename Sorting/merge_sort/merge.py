9d003ad3463e3053ee1c5f915c3d74e8bd087652
Pradyumna <pradyum2411@gmail.com>
2021-06-27 22:25:31 +0530

    MergeSortAdded

---
+++ Sorting/merge_sort/merge.py
@@ -0,0 +1,26 @@
+# Merge on a 0 - indexed mutable sub sequence A[1..n], where two sub sequences A[p..q] and A[q+1..r] are already sorted
+def merge(A, p, q, r):
+	L = []
+	R = []
+	for i in range(p, q+1):
+		L.append(A[i])
+	L.append(float('inf'))
+
+	for j in range(q+1, r+1):
+		R.append(A[j])
+	R.append(float('inf'))
+
+	i = 0
+	j = 0
+	for k in range(p, r+1):
+		if L[i] > R[j]:
+			A[k] = R[j]
+			j = j + 1
+		else:
+			A[k] = L[i]
+			i = i + 1
+
+#Simple merge test
+A = [0, 1, 4, 10, 2, 3, 9, 18] # p = 0, q = 3  and r = 7
+merge(A, 0, 3, 7)
+print(A) # [0, 1, 2, 3, 4, 9, 10, 18]
---
+++ Sorting/merge_sort/merge_sort.py
@@ -0,0 +1,12 @@
+from merge import merge
+def merge_sort(A, p, r):
+	if p < r:
+		q = (p + r) // 2
+		merge_sort(A, p, q)
+		merge_sort(A, q+1, r)
+		merge(A, p, q, r)
+
+# Simple test
+A = [3, 2, 9, 1, 0]
+merge_sort(A, 0, len(A)-1)
+print(A)
