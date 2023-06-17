e0c992dbe6b41e7cd786afdb68f5e9c7169b408e
Pradyumna Reddy <55283453+Pradyumna-Reddy@users.noreply.github.com>
2023-06-17 13:15:54 +0530

    Update WindowSlideFlipZeroes.cpp

--- WindowSlideFlipZeroes.cpp
+++ WindowSlideFlipZeroes.cpp
@@ -3,7 +3,7 @@ public:
     int longestOnes(vector<int>& nums, int k) {
         int i = 0,j = 0;
         int curr = 0;
-        int ans = 0;
+        //int ans = 0;
         
         for(int j = 0; j < nums.size(); j++) {
             if(nums[j] == 0) {
